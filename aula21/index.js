727c68f9d04bbed688d9fc2eb66cfbc2703e9c73
Leonardo Campos <leonardoworld@outlook.com>
2024-03-01 11:48:01 -0300

    Operadores Lógicos

---
+++ aula21/index.js
@@ -0,0 +1,19 @@
+/* 
+
+Operadores de comparação
+
+>   maior que
+>=  maior que ou igual a
+<   menor que
+<=  menor que ou igual a
+==  igualdade (valor) // Não vamos usar
+=== igualdade estrita (checa valor e tipo) // Usar esse
+!=  diferente (valor) // Não vamos usar
+!== difetente estrito (checa valor e tipo) // Usar esse
+
+*/
+
+const num1 = 10;
+const num2 = '10';
+const compara = num1 === num2;
+console.log(compara)
---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/* 
+
+Operadores Lógicos
+
+&&  ->  AND ->  E
+||  ->  OR  ->  OU
+!   ->  NOT ->  NÃO
+
+*/
+
+console.log(true && true); // As duas tem que ser verdadeira para retornar verdadeiro!
