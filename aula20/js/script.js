8c4b18544ac11b71aea47a9a0852dd59a0eebeb7
Leonardo Campos <leonardoworld@outlook.com>
2024-03-01 11:13:52 -0300

    Commit

---
+++ aula20/js/script.js
@@ -0,0 +1,32 @@
+function meuEscopo() {
+    const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = []
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+
+        const nome = form.querySelector('.input__nome')
+        const sobrenome = form.querySelector('.input__sobrenome')
+        const peso = form.querySelector('.input__peso')
+        const altura = form.querySelector('.input__altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+
+        console.log(pessoas)
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+}
+
+meuEscopo()
