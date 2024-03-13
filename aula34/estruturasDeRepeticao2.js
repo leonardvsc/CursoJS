ba29b3fc49f43921b2fa04003b4888ce8071f99e
Leonardo Campos <leonardoworld@outlook.com>
2024-03-13 17:46:12 -0300

    Create estruturasDeRepeticao2.js

---
+++ aula34/estruturasDeRepeticao2.js
@@ -0,0 +1,6 @@
+// Percorrer um array com for
+const frutas = ["maca", "pera", "uva", "melancia"];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Iremos consumir a fruta ${frutas[i]}`);
+}
