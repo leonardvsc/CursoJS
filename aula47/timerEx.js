2295b0f746e5497f806effe6f9b56a8c84d3537b
Leonardo Campos <leonardoworld@outlook.com>
2024-04-02 11:46:52 -0300

    Commit

--- aula47/timerEx.js
+++ aula47/timerEx.js
@@ -1,8 +1,27 @@
+function criaHoraDosSegundos(segundos) {
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString("pt-BR", {
+    hour12: false,
+    timeZone: "GMT",
+  });
+}
+
 const relogio = document.querySelector(".relogio");
 const iniciar = document.querySelector(".iniciar");
 const pausar = document.querySelector(".pausar");
 const zerar = document.querySelector(".zerar");
 
-iniciar.addEventListener("click", function (e) {});
-pausar.addEventListener("click", function (e) {});
-zerar.addEventListener("click", function (e) {});
+let segundos = 0;
+
+function iniciaRelogio() {
+  const timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = segundos;
+  }, 1000);
+}
+
+iniciar.addEventListener("click", (e) => {
+  iniciaRelogio();
+});
+pausar.addEventListener("click", (e) => {});
+zerar.addEventListener("click", (e) => {});
