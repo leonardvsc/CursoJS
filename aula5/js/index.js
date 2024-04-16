5d49c1ef41b8079a9742b276d769a631853b5bf5
Leonardo Campos <leonardoworld@outlook.com>
2024-04-15 23:29:32 -0300

    Update index.js

--- aula5/js/index.js
+++ aula5/js/index.js
@@ -1,4 +1,4 @@
 console.log("Oi");
-console.log("Esse trecho será exebido no console do navegador");
+console.log("Esse trecho será exibido no console do navegador");
 alert("Mensagem de alerta!");
 //Comentário
