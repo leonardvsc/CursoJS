0ea683e0cd1621dfd08fc4e143c9b02fdc503df3
Leonardo Campos <leonardoworld@outlook.com>
2024-04-20 00:01:35 -0300

    Create funcoesImediatasIIFE.js

---
+++ aula55/funcoesImediatasIIFE.js
@@ -0,0 +1,11 @@
+// IIEFE -> Immediately Invoked Function Expression
+// Função imediatamente invocada (IIFE)
+
+(function (idade, peso, altura) {
+  const nome = "Leonardo";
+
+  console.log(nome);
+  console.log(idade, peso, altura);
+})(30, 80, 1.8);
+
+const nome = "João";
