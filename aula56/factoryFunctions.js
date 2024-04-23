ebcb5c6765e0dd5bcdbe6b39ab2be329d696387a
Leonardo Campos <leonardoworld@outlook.com>
2024-04-22 23:30:56 -0300

    Update factoryFunctions.js

--- aula56/factoryFunctions.js
+++ aula56/factoryFunctions.js
@@ -1,14 +1,42 @@
-function criaPessoa(nome, sobrenome) {
+//Factory function
+function criaPessoa(nome, sobrenome, altura, peso) {
   return {
     nome,
     sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(" ");
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(" ");
+    },
+
     fala(assunto) {
       return `${this.nome} está ${assunto}.`;
     },
-    peso: 80,
+
+    altura,
+    peso,
+
+    // Getter
+    get imc() {
+      const i = this.peso / this.altura ** 2;
+      return i.toFixed(2);
+    },
   };
 }
 
-const p1 = criaPessoa("Leonardo", "Victor");
-const p2 = criaPessoa("Maria", "Joaquina");
-console.log(p2.fala());
+const p1 = criaPessoa("Leonardo", "Victor", 1.8, 80);
+p1.nomeCompleto = "Leonardo Victor Silva";
+const p2 = criaPessoa("Maria", "Joaquina", 1.6, 62);
+console.log(p1.nomeCompleto);
+
+//console.log(p1.imc);
+//console.log(p1.imc());
+//console.log(p1.fala("estudandando JavaScript"));
+//console.log(p2.fala("fazendo compras no mercado"));
