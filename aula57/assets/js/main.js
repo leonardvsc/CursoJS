2809b187bf98e6dafb31a7d59d7e29cad86340b3
Leonardo Campos <leonardoworld@outlook.com>
2024-04-23 17:52:41 -0300

    Update main.js

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -2,26 +2,39 @@ function criaCalculadora() {
   return {
     display: document.querySelector(".display"),
 
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
     inicia() {
       calculadora.cliqueBotoes();
     },
 
     cliqueBotoes() {
       // this
-      document.addEventListener(
-        "click",
-        function (e) {
-          const el = e.target;
+      document.addEventListener("click", (e) => {
+        const el = e.target;
 
-          if (el.classList.contains("btn-num")) {
-            this.btnParaDisplay();
-          }
-        }.bind(this)
-      );
+        if (el.classList.contains("btn-num")) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if (el.classList.contains("btn-clear")) {
+          calculadora.clearDisplay();
+        }
+
+        if (el.classList.contains("btn-del")) {
+          this.apagaUm();
+        }
+      });
     },
 
-    btnParaDisplay() {
-      alert("Você apertou um botão de numero");
+    btnParaDisplay(valor) {
+      this.display.value += valor;
     },
   };
 }
