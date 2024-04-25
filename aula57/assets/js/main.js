61a8c722db77ae22117ffd7c28643021d2012643
Leonardo Campos <leonardoworld@outlook.com>
2024-04-25 20:07:55 -0300

    Finished calculator

--- aula57/assets/js/main.js
+++ aula57/assets/js/main.js
@@ -2,16 +2,50 @@ function criaCalculadora() {
   return {
     display: document.querySelector(".display"),
 
+    inicia() {
+      this.cliqueBotoes();
+      this.pressionaEnter();
+    },
+
+    pressionaEnter() {
+      this.display.addEventListener("keyup", (e) => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+
+        if (e.keyCode === 27) {
+          this.clearDisplay();
+        }
+      });
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if (!conta) {
+          alert("Conta enválida");
+          return;
+        }
+
+        this.display.value = String(conta);
+      } catch (e) {
+        alert(
+          "Conta enválida que caiu no catch. O erro está exebido no console"
+        );
+        console.log(e);
+        return;
+      }
+    },
+
     clearDisplay() {
       this.display.value = "";
     },
 
     apagaUm() {
       this.display.value = this.display.value.slice(0, -1);
-    },
-
-    inicia() {
-      calculadora.cliqueBotoes();
     },
 
     cliqueBotoes() {
@@ -24,11 +58,15 @@ function criaCalculadora() {
         }
 
         if (el.classList.contains("btn-clear")) {
-          calculadora.clearDisplay();
+          this.clearDisplay();
         }
 
         if (el.classList.contains("btn-del")) {
           this.apagaUm();
+        }
+
+        if (el.classList.contains("btn-eq")) {
+          this.realizaConta();
         }
       });
     },
