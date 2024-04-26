017c016b56a244c0a73c12df5c92c5e76a04a211
Leonardo Campos <leonardoworld@outlook.com>
2024-04-25 22:44:58 -0300

    addNumDisplay -> this.display.focus()

--- aula59/assets/js/main.js
+++ aula59/assets/js/main.js
@@ -1,160 +1,60 @@
-/* function criaCalculadora() {
-  return {
-    display: document.querySelector(".display"),
-
-    inicia() {
-      this.cliqueBotoes();
-      this.pressionaEnter();
-    },
-
-    pressionaEnter() {
-      this.display.addEventListener("keyup", (e) => {
-        if (e.keyCode === 13) {
-          this.realizaConta();
-        }
-
-        if (e.keyCode === 27) {
-          this.clearDisplay();
-        }
-      });
-    },
-
-    realizaConta() {
-      let conta = this.display.value;
-
-      try {
-        conta = eval(conta);
-
-        if (!conta) {
-          alert("Conta enválida");
-          return;
-        }
-
-        this.display.value = String(conta);
-      } catch (e) {
-        alert(
-          "Conta enválida que caiu no catch. O erro está exebido no console"
-        );
-        console.log(e);
-        return;
-      }
-    },
-
-    clearDisplay() {
-      this.display.value = "";
-    },
-
-    apagaUm() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-
-    cliqueBotoes() {
-      // this
-      document.addEventListener("click", (e) => {
-        const el = e.target;
-
-        if (el.classList.contains("btn-num")) {
-          this.btnParaDisplay(el.innerText);
-        }
-
-        if (el.classList.contains("btn-clear")) {
-          this.clearDisplay();
-        }
-
-        if (el.classList.contains("btn-del")) {
-          this.apagaUm();
-        }
-
-        if (el.classList.contains("btn-eq")) {
-          this.realizaConta();
-        }
-      });
-    },
-
-    btnParaDisplay(valor) {
-      this.display.value += valor;
-    },
-  };
-}
-
-const calculadora = criaCalculadora();
-calculadora.inicia();
- */
-
-function CriarCalculadora() {
+function Calculadora() {
   this.display = document.querySelector(".display");
 
-  this.iniciar = () => {
-    this.cliqueBotoes();
-    this.pressionaEnter();
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaTeclas();
   };
 
-  this.pressionaEnter = () => {
+  this.capturaTeclas = () => {
     this.display.addEventListener("keyup", (e) => {
-      if (e.keyCode === 13) {
+      if (e.keyCode == 13) {
         this.realizaConta();
       }
-
-      if (e.keyCode === 27) {
-        this.clearDisplay();
+      if (e.keyCode == 27) {
+        this.clear();
       }
     });
   };
 
-  this.clearDisplay = () => {
-    this.display.value = "";
-  };
+  this.capturaCliques = () => {
+    document.addEventListener("click", (event) => {
+      const el = event.target;
 
-  this.apagaUm = () => {
-    this.display.value = this.display.value.slice(0, -1);
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clear();
+      if (el.classList.contains("btn-del")) this.del();
+      if (el.classList.contains("btn-eq")) this.realizaConta();
+    });
   };
 
   this.realizaConta = () => {
-    let conta = this.display.value;
-
     try {
-      conta = eval(conta);
+      const conta = eval(this.display.value);
 
       if (!conta) {
-        alert("Conta enválida");
+        alert("Conta inválida");
         return;
       }
 
-      this.display.value = String(conta);
+      this.display.value = conta;
     } catch (e) {
-      alert("Conta enválida que caiu no catch. O erro está exebido no console");
-      console.log(e);
+      alert("Conta inválida");
       return;
     }
   };
 
-  this.cliqueBotoes = () => {
-    document.addEventListener("click", (e) => {
-      const el = e.target;
-
-      if (el.classList.contains("btn-num")) {
-        this.btnParaDisplay(el.innerText);
-      }
-
-      if (el.classList.contains("btn-clear")) {
-        this.clearDisplay();
-      }
-
-      if (el.classList.contains("btn-del")) {
-        this.apagaUm();
-      }
-
-      if (el.classList.contains("btn-eq")) {
-        this.realizaConta();
-      }
-    });
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+    this.display.focus();
   };
 
-  this.btnParaDisplay = (valor) => {
-    this.display.value += valor;
+  this.clear = () => (this.display.value = "");
+  this.del = () => {
+    this.display.value = this.display.value.slice(0, -1);
   };
 }
 
-const calculadora = new CriarCalculadora();
-
-calculadora.iniciar();
+const calculadora = new Calculadora();
+console.log(calculadora);
+calculadora.inicia();
---
+++ aula59/assets/js/main2.js
@@ -0,0 +1,160 @@
+/* function criaCalculadora() {
+  return {
+    display: document.querySelector(".display"),
+
+    inicia() {
+      this.cliqueBotoes();
+      this.pressionaEnter();
+    },
+
+    pressionaEnter() {
+      this.display.addEventListener("keyup", (e) => {
+        if (e.keyCode === 13) {
+          this.realizaConta();
+        }
+
+        if (e.keyCode === 27) {
+          this.clearDisplay();
+        }
+      });
+    },
+
+    realizaConta() {
+      let conta = this.display.value;
+
+      try {
+        conta = eval(conta);
+
+        if (!conta) {
+          alert("Conta enválida");
+          return;
+        }
+
+        this.display.value = String(conta);
+      } catch (e) {
+        alert(
+          "Conta enválida que caiu no catch. O erro está exebido no console"
+        );
+        console.log(e);
+        return;
+      }
+    },
+
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    cliqueBotoes() {
+      // this
+      document.addEventListener("click", (e) => {
+        const el = e.target;
+
+        if (el.classList.contains("btn-num")) {
+          this.btnParaDisplay(el.innerText);
+        }
+
+        if (el.classList.contains("btn-clear")) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains("btn-del")) {
+          this.apagaUm();
+        }
+
+        if (el.classList.contains("btn-eq")) {
+          this.realizaConta();
+        }
+      });
+    },
+
+    btnParaDisplay(valor) {
+      this.display.value += valor;
+    },
+  };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
+ */
+
+function CriarCalculadora() {
+  this.display = document.querySelector(".display");
+
+  this.iniciar = () => {
+    this.cliqueBotoes();
+    this.pressionaEnter();
+  };
+
+  this.pressionaEnter = () => {
+    this.display.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        this.realizaConta();
+      }
+
+      if (e.keyCode === 27) {
+        this.clearDisplay();
+      }
+    });
+  };
+
+  this.clearDisplay = () => {
+    this.display.value = "";
+  };
+
+  this.apagaUm = () => {
+    this.display.value = this.display.value.slice(0, -1);
+  };
+
+  this.realizaConta = () => {
+    let conta = this.display.value;
+
+    try {
+      conta = eval(conta);
+
+      if (!conta) {
+        alert("Conta enválida");
+        return;
+      }
+
+      this.display.value = String(conta);
+    } catch (e) {
+      alert("Conta enválida que caiu no catch. O erro está exebido no console");
+      console.log(e);
+      return;
+    }
+  };
+
+  this.cliqueBotoes = () => {
+    document.addEventListener("click", (e) => {
+      const el = e.target;
+
+      if (el.classList.contains("btn-num")) {
+        this.btnParaDisplay(el.innerText);
+      }
+
+      if (el.classList.contains("btn-clear")) {
+        this.clearDisplay();
+      }
+
+      if (el.classList.contains("btn-del")) {
+        this.apagaUm();
+      }
+
+      if (el.classList.contains("btn-eq")) {
+        this.realizaConta();
+      }
+    });
+  };
+
+  this.btnParaDisplay = (valor) => {
+    this.display.value += valor;
+  };
+}
+
+const calculadora = new CriarCalculadora();
+
+calculadora.iniciar();
