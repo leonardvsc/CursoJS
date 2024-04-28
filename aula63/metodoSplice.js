2585ace0ac58a90188c69c5780d42e55213f2ff2
Leonardo Campos <leonardoworld@outlook.com>
2024-04-28 16:25:51 -0300

    Update metodoSplice.js

--- aula63/metodoSplice.js
+++ aula63/metodoSplice.js
@@ -7,6 +7,7 @@
 const nomes = ["Leonardo", "Maria", "João", "Luiza"];
 
 //                     splice(índice, deletar, adicionar);
-const removido = nomes.splice(-3, Number.MAX_VALUE, "Diogo");
+const removido = nomes.splice(-3, Number.MAX_VALUE, "Victor");
+// O splice remove elementos do array e adiciona novos elementos
 console.log(removido);
 console.log(nomes);
