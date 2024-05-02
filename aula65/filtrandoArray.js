5f754844c5330a934e00c433c7b56202e5eeb0ca
Leonardo Campos <leonardoworld@outlook.com>
2024-05-01 21:43:19 -0300

    Update filtrandoArray.js

--- aula65/filtrandoArray.js
+++ aula65/filtrandoArray.js
@@ -3,5 +3,7 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 // Filtrando os numeros maior que 10
-const numerosMaiorQue10 = numeros.filter((valor) => valor > 10);
+const numerosMaiorQue10 = numeros.filter(function (valor) {
+  return valor > 10;
+});
 console.log(numerosMaiorQue10);
