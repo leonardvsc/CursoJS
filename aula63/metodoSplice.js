2867d23599ff8d51d8c161ad7d644159fe59e3b6
Leonardo Campos <leonardoworld@outlook.com>
2024-05-08 15:51:12 -0300

    Update metodoSplice.js

--- aula63/metodoSplice.js
+++ aula63/metodoSplice.js
@@ -1,13 +1,9 @@
 // Metodo splice substitui o push, pop, unshift e shift
-// push() adiciona um elemento no final de um array
-// pop() remove o último elemento de um array
-// unshift() adiciona um elemento no comeco de um array
-// shift() remove o primeiro elemento de um array
-
 const nomes = ["Leonardo", "Maria", "João", "Luiza"];
 
 //                     splice(índice, deletar, adicionar);
 const removido = nomes.splice(-3, Number.MAX_VALUE, "Victor");
 // O splice remove elementos do array e adiciona novos elementos
+
 console.log(removido);
 console.log(nomes);
