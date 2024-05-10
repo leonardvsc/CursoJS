c1603252914d0536d821fbd70c23350aa746afd6
Leonardo Campos <leonardoworld@outlook.com>
2024-05-09 21:36:39 -0300

    Update gettersSetters.js

--- aula72/gettersSetters.js
+++ aula72/gettersSetters.js
@@ -9,9 +9,19 @@ function Carro(modelo, ano, placa) {
     get: function () {
       return placa;
     },
+    set: function (valor) {
+      if (typeof valor === "string") {
+        placa = valor;
+      } else {
+        throw new TypeError("Placa deve ser uma string");
+      }
+    },
   });
 }
 
 const carro1 = new Carro("Micro", 2010, "ABC-1234");
 console.log(carro1);
-console.log(carro1.pla);
+
+carro1.placa = "CNH-3233";
+
+console.log(carro1.placa);
