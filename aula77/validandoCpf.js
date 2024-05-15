5b6c88fec1fa98e454033e8b5b6c6ab3794c47cf
Leonardo Campos <leonardoworld@outlook.com>
2024-05-14 22:05:04 -0300

    Update validandoCpf.js

--- aula77/validandoCpf.js
+++ aula77/validandoCpf.js
@@ -7,13 +7,22 @@
 7x | 0x | 5x | 4x | 8x | 4x | 4x | 5x | 0x
 x  | x  | x  | x  | x  | x  | x  | x  | x
 10 | 9  | 8  | 7  | 6  | 5  | 4  | 3  | 2
+
 70 | 0  | 40 | 28 | 48 | 20 | 16 | 15 | 0 = 237
 
+11 - (237 % 11) = 5 (Primeiro digito)
 
+7x | 0x | 5x | 4x | 8x | 4x | 4x | 5x | 0x | 5x
+x  | x  | x  | x  | x  | x  | x  | x  | x  | x
+11 | 10 | 9  | 8  | 7  | 6  | 5  | 4  | 3  | 2
+
+77 | 0  | 45 | 32 | 56 | 24 | 20 | 20 | 0  | 5 = 284
+
+11 - (284 % 11) = 2 (Segundo digito)
 
 */
 
-function ValidaCPF(cpfEnviado) {
+function ValidaCpf(cpfEnviado) {
   Object.defineProperty(this, "cpfLimpo", {
     enumerable: true,
     get: function () {
@@ -22,7 +31,38 @@ function ValidaCPF(cpfEnviado) {
   });
 }
 
-ValidaCPF.prototype.valida = function () {};
+ValidaCpf.prototype.valida = function () {
+  if (typeof this.cpfLimpo === "undefined") return false;
+  if (this.cpfLimpo.length !== 11) return false;
+  if (this.isSequencia()) return false;
 
-const cpf = new ValidaCPF("705.484.450-52");
-cpf.valida();
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+  const digito2 = this.criaDigito(cpfParcial + digito1);
+
+  const novoCpf = cpfParcial + digito1 + digito2;
+
+  return novoCpf === this.cpfLimpo;
+};
+
+ValidaCpf.prototype.criaDigito = function (cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+
+  let regressivo = cpfArray.length + 1;
+  const total = cpfArray.reduce((ac, val) => {
+    ac += regressivo * Number(val);
+    regressivo--;
+    return ac;
+  }, 0);
+
+  const digito = 11 - (total % 11);
+  return digito > 9 ? "0" : String(digito);
+};
+
+ValidaCpf.prototype.isSequencia = function () {
+  const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+  return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCpf("705.484.450-52");
+console.log(cpf.valida());
