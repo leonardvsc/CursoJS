2b6fa0e4b33b10decbed47877c9435b10d426f0c
Leonardo Campos <leonardoworld@outlook.com>
2024-05-18 21:56:53 -0300

    Create metodosUteisParaPromises.js

---
+++ aula87/metodosUteisParaPromises.js
@@ -0,0 +1,16 @@
+function tempoAlatorio(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    if (typeof msg !== "string") reject(false);
+
+    setTimeout(() => {
+      console.log(msg);
+      resolve(msg);
+    }, tempo);
+  });
+}
