5cc9a078a15b8092d3f58ce0011d3f07ecbae7dd
Leonardo Campos <leonardoworld@outlook.com>
2024-05-20 05:18:20 -0300

    Update asyncAwait.js

--- aula88/asyncAwait.js
+++ aula88/asyncAwait.js
@@ -17,13 +17,28 @@ function esperaAi(msg, tempo) {
   });
 }
 
-esperaAi("Frase 1", tempoAlatorio(1, 3))
-  .then(() => {
-    return esperaAi("Frase 2", tempoAlatorio(1, 3));
-  })
-  .then(() => {
-    return esperaAi("Frase 3", tempoAlatorio(1, 3));
-  })
-  .catch((e) => {
+async function executar() {
+  try {
+    const fase1 = await esperaAi("Fase 1", tempoAlatorio(1, 3));
+    console.log(fase1);
+    const fase2 = await esperaAi("Fase 2", tempoAlatorio(1, 3));
+    console.log(fase2);
+    const fase3 = await esperaAi("Fase 3", tempoAlatorio(1, 3));
+    console.log(fase3);
+  } catch (e) {
     console.log(e);
-  });
+  }
+}
+
+executar();
+
+// esperaAi("Frase 1", tempoAlatorio(1, 3))
+//   .then((valor) => {
+//     console.log(valor);
+//     return esperaAi("Frase 2", tempoAlatorio(1, 3));
+//   })
+//   .then((valor) => {
+//     console.log(valor);
+//     return esperaAi("Frase 3", tempoAlatorio(1, 3));
+//   })
+//   .catch((erro) => console.log(erro));
